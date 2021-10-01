04a7fb4bfc2946475bfc039fd1300c2376d235c4
MohamedHafez13 <muhammedhaffezz@gmail.com>
2021-10-01 14:05:35 +0200

    observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/modules/splash_screen/splash_screen.dart';
+import 'package:food_app/shared/observer.dart';
 import 'layout/my_home_page.dart';
 import 'modules/home/home.dart';
 import 'modules/user_profile/profile_screen.dart';
@@ -8,6 +10,10 @@ import 'modules/user_profile/profile_screen.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+    Bloc.observer = MyBlocObserver();
+    // Use cubits...
+
   runApp(MyApp());
 }
 
